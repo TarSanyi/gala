0ae333ef02d310a59512efe49bf5bf1257fb33a0
Dani Haluska <rhacker339@gmail.com>
2026-05-19 22:33:34 +0200

    F

--- 2/src/Navbar.jsx
+++ 2/src/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success shadow-sm">
             <div className="container">
                 <Link className="navbar-brand" to="/">Szállások</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
